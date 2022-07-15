239c3b12e70490bce8405e0e145489514bc561b2
norwd <106889957+norwd@users.noreply.github.com>
2022-07-15 12:22:28 +1200

    Reformat documentation of formatting directives

--- format/directive.go
+++ format/directive.go
@@ -4,21 +4,58 @@ package format
 type Directive string
 
 const (
-	FullWeekdayDirective  Directive = "%A" // Formats the full name of the day of the week (i.e. Sweetmorn).
-	AbbrWeekdayDirective  Directive = "%a" // Formats the abbreviated name of the day of the week (i.e. SM).
-	FullSeasonDirective   Directive = "%B" // Formats the full name of the season (i.e. Chaos).
-	AbbrSeasonDirective   Directive = "%b" // Formats the abbreviated name of the day of the week (i.e. Chs).
-	OrdinalDayDirective   Directive = "%d" // Formats the ordinal number of the day in the season (i.e. 23).
-	CardinalDayDirective  Directive = "%e" // Formats the cardinal number of the day in the season (i.e. 23rd).
-	OrdinalYearDirective  Directive = "%Y" // Formats the ordinal year of our lady of of discord (i.e. 3161).
-	CardinalYearDirective Directive = "%y" // Formats the cardinal year of our lady of discord (i.e. 3161st).
-	HolydayDirective      Directive = "%H" // Formats the name of the current Holyday, if any (i.e. Confuflux).
-	NonHolidayDirective   Directive = "%N" // Is a magic code to prevent the remainder of the format string being printed unless the date is a Holyday.
-	NewlineDirective      Directive = "%n" // Formats a newline character.
-	TabDirective          Directive = "%t" // Formats a tab character.
-	PercentDirective      Directive = "%%" // Formats a literal percent sign character.
-	XDayDirective         Directive = "%X" // Formats the cardinal number of days remaining until X-Day from the given date.
-	StartTibsDayDirective Directive = "%{" // Begins a special block to enclose part of the string.
-	EndTibsDayDirective   Directive = "%}" // Closes a special block opened by %}, the enclosed the part of the string is replaced with the words "St. Tib's Day" if the current day is St. Tib's Day.
-	MagicDirective        Directive = "%." // Try it and see...
+	// Formats the full name of the day of the week (i.e. Sweetmorn).
+	FullWeekdayDirective Directive = "%A"
+
+	// Formats the abbreviated name of the day of the week (i.e. SM).
+	AbbrWeekdayDirective Directive = "%a"
+
+	// Formats the full name of the season (i.e. Chaos).
+	FullSeasonDirective Directive = "%B"
+
+	// Formats the abbreviated name of the day of the week (i.e. Chs).
+	AbbrSeasonDirective Directive = "%b"
+
+	// Formats the ordinal number of the day in the season (i.e. 23).
+	OrdinalDayDirective Directive = "%d"
+
+	// Formats the cardinal number of the day in the season (i.e. 23rd).
+	CardinalDayDirective Directive = "%e"
+
+	// Formats the ordinal year of our lady of of discord (i.e. 3161).
+	OrdinalYearDirective Directive = "%Y"
+
+	// Formats the cardinal year of our lady of discord (i.e. 3161st).
+	CardinalYearDirective Directive = "%y"
+
+	// Formats the name of the current Holyday, if any (i.e. Confuflux).
+	HolydayDirective Directive = "%H"
+
+	// Is a magic code to prevent the remainder of the format string being
+	// printed unless the date is a Holyday.
+	NonHolidayDirective Directive = "%N"
+
+	// Formats a newline character.
+	NewlineDirective Directive = "%n"
+
+	// Formats a tab character.
+	TabDirective Directive = "%t"
+
+	// Formats a literal percent sign character.
+	PercentDirective Directive = "%%"
+
+	// Formats the cardinal number of days remaining until X-Day from the given
+	// date.
+	XDayDirective Directive = "%X"
+
+	// Begins a special block to enclose part of the string.
+	StartTibsDayDirective Directive = "%{"
+
+	// Closes a special block opened by %}, the enclosed the part of the string
+	// is replaced with the words "St. Tib's Day" if the current day is St.
+	// Tib's Day.
+	EndTibsDayDirective Directive = "%}"
+
+	// Try it and see...
+	MagicDirective Directive = "%."
 )
